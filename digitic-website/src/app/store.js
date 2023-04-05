82a10b49161bba17fe81d0964e118ae391ac8a34
harsh-weboccult <harsh.parmar@weboocult.com>
2023-04-05 19:10:20 +0530

    "complated till conract form "

--- digitic-website/src/app/store.js
+++ digitic-website/src/app/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/user/userSlice";
-import  productReducer  from "../features/products/productSlice";
+import productReducer from "../features/products/productSlice";
+import blogReducer from "../features/blogs/blogeSlice";
+import contactReducer from "../features/contect/contactSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    product:productReducer
+    product: productReducer,
+    blog: blogReducer,
+    contact: contactReducer,
   },
 });
